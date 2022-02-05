554ffbbdbb496b272ef26402f731e608cb5c7b65
Thibaud Maurel <maurel.thibaud@free.fr>
2022-02-05 17:06:30 +0100

    gift_list completed

--- christmas_list/christmas.rb
+++ christmas_list/christmas.rb
@@ -1,3 +1,42 @@
 # christmas.rb
 
 # STEP2.5: Refacto the list action into a method
+# STEP5: Update the list method to display the gift name & if it was bought or not
+def list(gift_list)
+  # STEP2: Iterate over the gift list to print every single gift
+  gift_list.each_with_index do |gift, index|
+    cross = gift[:bought] ? "X" : " "
+    puts "#{index + 1} - [#{cross}] #{gift[:name]}"
+  end
+end
+
+# STEP3.5: Refacto the add action into a method
+def add(gift_list)
+  # STEP3: Ask the user which gift he wants to add
+  puts "Which gift do you want to add?"
+  # STEP3: Store his answer into a variable
+  add = gets.chomp
+  # STEP3: Add the new gift to our gift_list
+  # STEP5: Update what we push inside the gift list to make it a hash (and by default bought: false)
+  gift_list << { name: add, bought: false }
+end
+
+# STEP4.5: Refacto the delete action into a method
+def delete(gift_list)
+  # STEP4: Ask the user what he wants to delete (index)
+  puts "Which item do you want to delete? (give number)"
+  # STEP4: Store his answer into a variable
+  index_to_delete = gets.chomp.to_i - 1
+  # STEP4: Delete his answer from the gift list
+  gift_list.delete_at(index_to_delete)
+end
+
+# STEP 5.5: Refacto the mark action into a method
+def mark(gift_list)
+  # STEP 5.5: Ask the user which item he bought (index)
+  puts "Which item have you bought (give the index)?"
+  # STEP 5.5: Store his answer into a variable
+  index_mark = gets.chomp.to_i - 1
+  # STEP 5.5: Update our gift list to take his answer into account
+  gift_list[index_mark][:bought] = true
+end
--- christmas_list/interface.rb
+++ christmas_list/interface.rb
@@ -1,26 +1,64 @@
 # interface.rb
+require_relative 'christmas'
+require 'open-uri'
+require 'nokogiri'
 
 # STEP2: Define the gift list (as an array)
+# gift_list = [
+#   "sockets",
+#   "ruby book",
+#   "macbook pro"
+# ]
+
+# STEP5: Update our gift_list variable
+gift_list =[
+  { name: "sockets", bought: false },
+  { name: "ruby book", bought: true},
+  { name: "macbook pro", bought: false }
+]
+
+
 # Display a welcome message
 puts "Welcome to your Christmas gift list"
 # Loop until the user wants to quit
 answer = ""
 until answer == "quit"
   # Ask the user which action he wants to perform
-  puts "Which action [list|add|delete|quit]?"
+  puts "Which action [list|add|delete|mark|idea|quit]?"
   # Store his answer into a variable
   answer = gets.chomp
   # Depending on the answer, print a message of the action to perform
   case answer
   when "list"
-    # puts "TODO: list items"
-    # STEP2: Iterate over the gift list to print every single gift
+    list(gift_list)
   when "add"
-    puts "TODO: add item"
+    add(gift_list)
   when "delete"
-    puts "TODO: delete item"
+    delete(gift_list)
+  # STEP 5.5: Add a new case to mark a gift as bought
+  when "mark"
+    mark(gift_list)
   when "quit"
     puts "Goodbye"
+  when "idea"
+    puts "What are you looking for on Etsy?"
+    idea = gets.chomp
+    puts "Here are Etsy results for \"#{idea}\":"
+    url = "https://www.etsy.com/fr/search?q=#{idea}"
+    html_content = URI.open(url).read
+    doc = Nokogiri::HTML(html_content)
+    suggestions = []
+    doc.search('.v2-listing-card__title').first(4).each do |element|
+      suggestions << element.text.strip
+    end
+    suggestions.each_with_index do |suggestion, index|
+      puts "#{index + 1} - #{suggestion}"
+    end
+    puts "Pick one to add to your list (give the number)"
+    index_to_add = gets.chomp.to_i - 1
+    item_to_add = suggestions[index_to_add]
+    gift_list << {name: item_to_add, bought: false}
+    puts "\"#{item_to_add}\" added to your wishlist"
   end
 # End the loop
 end
